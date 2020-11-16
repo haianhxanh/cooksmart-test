4634edd4c24e67277b4b942bc53a3f7f62cc24c1
haianhxanh <hanka7n@gmail.com>
2020-11-16 16:46:11 +0100

    styling

--- resources/views/banner/banner.blade.php
+++ resources/views/banner/banner.blade.php
@@ -3,8 +3,10 @@
 
 
 <div id="app" >
-    <div class="logo"></div>
-        <div class="slogan">
+    <div class="logo">
+        <a href="{{action('RecipeController@create')}}"><img src="https://www.dropbox.com/s/1znnm2hkyaw6w7d/logo-large.png?raw=1" alt=""></a>
+    </div>
+    <div class="slogan">
         <h2>Smart recipes for dump people who are lazy to think</h2>
     </div>
     <div class="btn">
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <link rel="icon" type="image/svg" href="https://www.dropbox.com/s/c52ver1mvfjjtns/favicon.svg?raw=1"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <title>Cook Smart</title>
 </head>
 <body>
