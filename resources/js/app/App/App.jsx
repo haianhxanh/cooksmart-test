422cd89e1e1397038b8774c9b48a4c9929026b9e
annie2527 <you@example.com>
2020-11-16 14:46:16 +0100

    updates

---
+++ resources/js/app/App/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Banner from './Banner/Banner.jsx';
+
+function App() {
+    return (
+        <Banner />
+    )
+}
+export default App
---
+++ resources/views/banner/banner.blade.php
@@ -0,0 +1,5 @@
+<div id="app" >
+    <h1>This is a test</h1>
+    
+</div>
+{{-- <script src="{{ mix('js/banner.js') }}"></script> --}}
