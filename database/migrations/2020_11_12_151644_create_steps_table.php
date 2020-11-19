33f33570c75d52298ea0277573dd81968a8dd221
annie2527 <you@example.com>
2020-11-19 21:41:39 +0100

    seeders and migration

--- database/migrations/2020_11_12_151644_create_steps_table.php
+++ database/migrations/2020_11_12_151644_create_steps_table.php
@@ -15,7 +15,7 @@ class CreateStepsTable extends Migration
     {
         Schema::create('steps', function (Blueprint $table) {
             $table->id();
-            $table->string('step');
+            $table->text('step');
             $table->unsignedBigInteger('number');
             $table->unsignedBigInteger('recipe_id');
             $table->timestamps();
