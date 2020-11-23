6c9f2044455a5d969c0ca81ef470918bf3db002f
annie2527 <you@example.com>
2020-11-23 10:00:31 +0100

    search

--- resources/js/app/Search/Search.jsx
+++ resources/js/app/Search/Search.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import SearchResults from './SearchResults.jsx';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import '../../../scss/search.scss';
 
 function Search() {
     const [cuisine, setCuisine] = useState("");
@@ -48,54 +49,68 @@ function Search() {
     };
 
     return (
-        <div className="search-bar">
-            <form className="form-group" 
-                onSubmit={(e) => { 
-                    e.preventDefault();
-                    searchRecipes()
-                }}>
-                <label className="label" htmlFor="time">
-                    Time
-                </label>
-                <select className="drop-down" name="time" onChange={selectTime}>
-                    <option value="">I have nothing but time!</option>
-                    <option value="30">30 minutes</option>
-                    <option value="60">60 minutes</option>
-                    <option value="90">90 minutes</option>
-                </select>
+        <div className="container">
+            <div className="search-bar">
+                <h1 className="title">Find a Recipe</h1>
+                <form className="form-group" 
+                    onSubmit={(e) => { 
+                        e.preventDefault();
+                        searchRecipes()
+                    }}>
+                    <span className="category">
+                    <label className="label" htmlFor="time">
+                        Time
+                    </label>
+                    <img className="category-img" src="/images/time.png" height="30px" width="30px"/>
+                    <select className="drop-down" name="time" onChange={selectTime}>
+                        <option value="">I have nothing but time!</option>
+                        <option value="30">30 minutes</option>
+                        <option value="60">60 minutes</option>
+                        <option value="90">90 minutes</option>
+                    </select>
+                    </span>
 
-                <label className="label" htmlFor="cuisine">
-                    Cuisine
-                </label>
-                <select
-                    className="drop-down"
-                    name="cuisine"
-                    onChange={selectCuisine}
-                >
-                    <option value="">Pick the most delicious one!</option>
-                    <option value="Western">Western</option>
-                    <option value="Asian">Asian</option>
-                    <option value="Fusion">Fusion</option>
-                    <option value="Middle Eastern">Middle Eastern</option>
-                    <option value="African">African</option>
-                    <option value="Latin American">Latin American</option>
-                </select>
+                    <span className="category">
+                    <label className="label" htmlFor="cuisine">
+                        Cuisine
+                    </label>
+                    <img className="category-img" src="/images/cuisine.png" height="30px" width="30px"/>
+                    <select
+                        className="drop-down"
+                        name="cuisine"
+                        onChange={selectCuisine}
+                    >
+                        <option value="">Pick the most delicious one!</option>
+                        <option value="Western">Western</option>
+                        <option value="Asian">Asian</option>
+                        <option value="Fusion">Fusion</option>
+                        <option value="Middle Eastern">Middle Eastern</option>
+                        <option value="African">African</option>
+                        <option value="Latin American">Latin American</option>
+                    </select>
+                    </span>
 
-                <label className="label" htmlFor="diet">
-                    Diet
-                </label>
-                <select className="drop-down" name="diet" onChange={selectDiet}>
-                    <option value="">Suprise me!</option>
-                    <option value="vegetarian">Vegetarian</option>
-                    <option value="pescatarian">Pescatarian</option>
-                    <option value="dairy-free">Dairy-free</option>
-                </select>
+                    <span className="category">
+                    <label className="label" htmlFor="diet">
+                        Diet
+                    </label>
+                    <img className="category-img" src="/images/diet.png" height="30px" width="30px"/>
+                    <select className="drop-down" name="diet" onChange={selectDiet}>
+                        <option value="">Suprise me!</option>
+                        <option value="vegetarian">Vegetarian</option>
+                        <option value="pescatarian">Pescatarian</option>
+                        <option value="dairy-free">Dairy-free</option>
+                    </select>
+                    </span>
 
-                <button type="submit">Take me to the delicious recipes</button>
-            </form>
+                    <span className="category">
+                    <button className="recipe-btn" type="submit">Take me to the recipes!</button>
+                    </span>
+                </form>
 
-                <SearchResults recipes={recipes}/>
+                    <SearchResults recipes={recipes}/>
 
+            </div>
         </div>
     );
 }
