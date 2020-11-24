5ef00d7d8efdf4e2261c3e88490a7ea5e9f92cda
annie2527 <you@example.com>
2020-11-24 10:25:35 +0100

    update

--- resources/js/app/App/App.jsx
+++ resources/js/app/App/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner.jsx';
 import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
 import Search from '../Search/Search.jsx';
-// import About from '../About/About.jsx';
+import About from '../About/About.jsx';
 
 
 function App() {
@@ -14,9 +14,9 @@ function App() {
                 <Route path="/search" > 
                     <Search />
                 </Route>
-                {/* <Route path="/about" >
+                <Route path="/about" >
                     <About />
-                </Route> */}
+                </Route>
             </Switch>
         </Router>
 
