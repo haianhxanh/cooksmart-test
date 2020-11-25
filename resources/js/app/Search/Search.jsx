3dcaf921cc632705c39dc2fb0cf50842859a9208
Jordan Domovcijski <jordan.domovcijski@gmail.com>
2020-11-25 14:55:36 +0100

    adjalj

--- resources/js/app/Search/Search.jsx
+++ resources/js/app/Search/Search.jsx
@@ -153,17 +153,19 @@ function Search() {
                     </span>
                 </form>
 
-<<<<<<< HEAD
                 <SearchResults recipes={recipes} />
-=======
-                    <SearchResults recipes={recipes}/>
-                    
-                    {
-                // moved outside of div, if doesn't work, move above last line
-                        hasMore === true ? 
-                        <button className="add-more" onClick={() => setPage(page + 1)}>Find more</button> : ''
-                    }
->>>>>>> c298f25da2c2fde0b3ea727124b7085e8337da4a
+
+                {// moved outside of div, if doesn't work, move above last line
+                hasMore === true ? (
+                    <button
+                        className="add-more"
+                        onClick={() => setPage(page + 1)}
+                    >
+                        Find more
+                    </button>
+                ) : (
+                    ""
+                )}
 
                 {hasMore === true ? (
                     <button
@@ -181,7 +183,6 @@ function Search() {
                     ""
                 )}
             </div>
-           
         </div>
     );
 }
