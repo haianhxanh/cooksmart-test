4ef60c88d77bfe0536b6b8f914273be89faaba1e
annie2527 <you@example.com>
2020-11-25 09:37:23 +0100

    update

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -1,20 +1,36 @@
 @extends('layouts.main')
+@include('layouts.navigation')
 @section('content')
-<h1>List of Recipes</h1>
+<div class="recipe-container">
+    <div class="index-header">
+        <h1>List of Recipes</h1>
+    </div>
 
-    @if (Session::has('recipe_success_message'))
-    <div class=“alert alert--success”>{{ Session::get('recipe_success_message') }}</div>
+     @if (Session::has('recipe_success_message'))
+        <div id="success-message" class=“alert alert--success”>{{ Session::get('recipe_success_message') }}</div>
     @endif
 
+    <div class="recipe-table">
+       
+    <table>
+        <thead>
+        <tr>
+            <th>Recipe Name</th>
+            <th>Image</th>
+            <th>Edit Recipe</th>
+        </tr>
+        </thead>
+        <tbody>
     @foreach ($recipes as $r)
-    <h1>{{ $r->name }}</h1>
+        <tr class="recipe-table-row">
+            <td class="recipe-table-name">{{ $r->name }}</td>
+            <td class="recipe-table-img"><img src="{{ $r->image_url }}" alt="{{$r->name}}"></td>
+            <td><button class="edit-btn">Edit Recipe</button></td>
+        </tr>
+    </tbody>
 
-    <h2>{{ $r->total_time_id }}</h2>
-
-    <p>{{ $r->description }}</p>
-
-    <div>{{ $r->video_url }}</div>
-
-    <img src="{{ $r->image_url }}" alt="">
     @endforeach
+    </table>
+</div>
 @endsection
+</div>
