beaecce6501d122c34d10e80581d147fab5a48b2
Jordan Domovcijski <jordan.domovcijski@gmail.com>
2020-11-26 17:20:26 +0100

    head fixed

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,11 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-<<<<<<< HEAD
-    <link rel="icon" type="image/svg" href="https://www.dropbox.com/s/c52ver1mvfjjtns/favicon.svg?raw=1"/>
-=======
     <link rel="icon" type="svg" href="{{ asset('img/logo-large.png') }}"/>
->>>>>>> melissa
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
